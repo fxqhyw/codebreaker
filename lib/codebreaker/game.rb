48fbc43f506b9a36ad18a0003f4718240f3a0030
therealloker <therealloker@gmail.com>
2018-05-29 18:20:00 +0300

    user code validation, private generate method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -3,17 +3,29 @@ module Codebreaker
     attr_accessor :atempts
 
     def initialize
-      @atempts = 10
-      @code = generate
+      @available_atempts = 10
+      @used_hints = 0
+      @secret_code = generate
     end
+
+    def hint
+      @used_hints += 1
+      index = rand(0..3)
+      @secret_code[index]
+    end
+
+    def make_guess(user_code)
+      return 'Incorrect format' unless code_valid?(user_input)
+    end
+
+    private
 
     def generate
       Array.new(4) { rand(1..6) }
     end
 
-    def hint
-      index = rand(0..3)
-      @secret_code[index]
+    def code_valid?(user_code)
+      user_code.match(/^[1-6]{4}$/)
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,14 +4,31 @@ module Codebreaker
   RSpec.describe Game do
     context '#generate' do
       it 'returns secret code array of 4 numbers from 1 to 6' do
-        expect(subject.generate.join).to match(/^[1-6]{4}$/)
+        expect(subject.send(:generate).join).to match(/^[1-6]{4}$/)
       end
     end
-    
+
     context '#hint' do
       it 'returns one random number from the secret code' do
-        subject.instance_variable_set(:@secret_code, '1236')
-        expect(subject.hint).to match(/^[1-6]{1}$/)
+        subject.instance_variable_set(:@secret_code, [1, 2, 3, 6])
+        expect(subject.hint.to_s).to match(/^[1-6]{1}$/)
+      end
+
+      it 'increases hints counter by 1' do
+        subject.instance_variable_set(:@used_hints, 0)
+        subject.hint
+        expect(subject.instance_variable_get(:@used_hints)).to eq(1)
+      end
+    end
+    context '#code_valid?' do
+      let(:valid_code) { '1236' }
+      let(:invalid_code) { 'invalid code, lol' }
+      it 'returns true if user code is valid' do
+        expect(subject.send(:code_valid?, valid_code)).to be_truthy
+      end
+
+      it 'returns false if user code is invalid' do
+        expect(subject.send(:code_valid?, invalid_code)).to be_falsey
       end
     end
   end
